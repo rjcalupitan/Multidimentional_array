95d2595e623a8f928285cd336c765b3c6db34a40
rjcalupitan <35456686+rjcalupitan@users.noreply.github.com>
2018-02-18 12:08:49 +0800

    Add files via upload

---
+++ multidimentional_arrray.py
@@ -0,0 +1,32 @@
+list = " The quick bRowN FoX jUmPs oVeR thE LaZy dOg "
+print(list)
+
+list1 = ["T","h","e", "q","u","i","c","k", "b","R","o","w","N", "F","o","X", "j","U","m","P","s", "o","V","e","R", "t","h","E", "L","a","Z","y", "d","O","g"]
+
+item1 = list1[0]
+item2 = list1[9]
+item3 = list1[12]
+item4 = list1[13]
+item5 = list1[15]
+item6 = list1[17]
+item7 = list1[19]
+item8 = list1[22]
+item9 = list1[24]
+item10 = list1[27]
+item11 = list1[28]
+item12 = list1[30]
+item13 = list1[33]
+
+print (item1)
+print (item2)
+print (item3)
+print (item4)
+print (item5)
+print (item6)
+print (item7)
+print (item8)
+print (item9)
+print (item10)
+print (item11)
+print (item12)
+print (item13)
